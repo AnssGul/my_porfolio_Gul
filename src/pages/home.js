23a8469706fb9e44b587777efdb2df0e710abe72
Muhammad Anus <muhammad.anus@geekybugs.com>
2025-01-02 18:23:38 +0500

    update ui

--- src/pages/home.js
+++ src/pages/home.js
@@ -28,8 +28,8 @@ const Home = () => {
                     src="/profile.jpg"
                     alt="Muhammad Anus Gul"
                     style={{
-                        width: '200px',
-                        height: '200px',
+                        width: '202px',
+                        height: '202px',
                         borderRadius: '50%',
                         marginBottom: '20px',
                         boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
